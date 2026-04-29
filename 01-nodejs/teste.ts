d805207f3b673899692b3e13983f278760ea1f2e
William Barbosa <wsbprofissional@gmail.com>
2026-04-28 22:30:08 -0300

    Aulas node

--- 01-nodejs/teste.ts
+++ 01-nodejs/teste.ts
@@ -1,4 +1,5 @@
-let nome: string = "William";
+let nome: string = "William Sousa Barbosa";
 let idade: number = 33;
 
-let fullName = `Nome completo : ${nome} ${idade}`;
+let fullName = `Nome completo : ${nome} idade: ${idade}`;
+console.log(fullName);
