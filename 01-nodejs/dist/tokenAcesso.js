37753147c527cc0ba2117d4b601987cf83ffa28e
William Barbosa <wsbprofissional@gmail.com>
2026-05-25 19:12:11 -0300

    reset

--- 01-nodejs/dist/tokenAcesso.js
+++
@@ -1,8 +0,0 @@
-// Usando Token de Acesso!
-let nome = "William Sousa Barbosa";
-let idade = 33;
-let token = process.env.TOKEN_DE_ACESSO;
-let fullName = `Nome completo : ${nome} idade: ${idade}`;
-console.log(fullName);
-console.log(`TOKEN DE ACESSO: ${token} `);
-export {};
